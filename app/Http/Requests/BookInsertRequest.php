126503563ba218773283b14c63767941b347b1d4
Shun Okawa <shunokawa@gmail.com>
2023-06-10 11:32:22 +0900

    feat: バリデーションメッセージを登録

--- app/Http/Requests/BookInsertRequest.php
+++ app/Http/Requests/BookInsertRequest.php
@@ -29,4 +29,17 @@ class BookInsertRequest extends FormRequest
             'memo' => 'nullable|string|max:1000',
         ];
     }
+
+    public function messages()
+    {
+        return [
+            'user_id.required' => 'ユーザーIDは必須です。',
+            'user_id.integer' => 'ユーザーIDは整数で入力してください。',
+            'title.required' => 'タイトルは必須です。',
+            'title.string' => 'タイトルは文字列で入力してください。',
+            'title.max' => 'タイトルは255文字以内で入力してください。',
+            'memo.string' => 'メモは文字列で入力してください。',
+            'memo.max' => 'メモは1000文字以内で入力してください。',
+        ];
+    }
 }
